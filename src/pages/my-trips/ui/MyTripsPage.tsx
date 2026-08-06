bf0ade182cd082f74ba4736e9e97a4ba6a1ac8ea
Dzmitry Poltaradnev <poltaradnev@gmail.com>
2026-08-06 17:47:29 +0400

    feat: create MyTrips page layout

--- src/pages/my-trips/ui/MyTripsPage.tsx
+++ src/pages/my-trips/ui/MyTripsPage.tsx
@@ -1,8 +1,30 @@
+import s from './MyTripsPage.module.scss'
+import {BackArrowIcon} from "@/widgets/country-details/ui/icons/BackArrow.tsx";
+import {TripsList} from "@/widgets/trips-list/ui/TripsList.tsx";
+
 export const MyTripsPage = () => {
     return (
-        <div>
-            MyTripsPage
-        </div>
+        <>
+            <div className={s.myTripsPageHeader}>
+                <div className={s.headerFirstSection}>
+                    <button className={s.backArrow}>
+                        <BackArrowIcon/>
+                    </button>
+                    <p className={s.myTripsPageTitle}>All Trips</p>
+                </div>
+                <div className={s.headerSecondSection}>
+                    <p className={s.myTripsPageCounter}>6</p>
+                </div>
+            </div>
+            <ul className={s.myTripsTypeList}>
+                <li className={s.myTripsListItem}>all</li>
+                <li className={s.myTripsListItem}>upcoming</li>
+                <li className={s.myTripsListItem}>current</li>
+                <li className={s.myTripsListItem}>past</li>
+            </ul>
+            <button>Plan a new trip</button>
+            <TripsList/>
+        </>
     );
 };
 
---
+++ src/widgets/trips-list/ui/TripsList.tsx
@@ -0,0 +1,12 @@
+import {TripCardByTripPage} from "@/entities/trip-card-by-trip-page/ui/TripCardByTripPage.tsx";
+
+export const TripsList = () => {
+    return (
+        <div>
+            <TripCardByTripPage/>
+            <TripCardByTripPage/>
+            <TripCardByTripPage/>
+        </div>
+    );
+};
+
